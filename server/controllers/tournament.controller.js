5cf8e8be92c564f0515279d96c978afde7a25ec2
oscarg11 <oscar.g2511@gmail.com>
2023-12-27 19:11:40 -0500

    added update and delete function to tournament controller

--- server/controllers/tournament.controller.js
+++ server/controllers/tournament.controller.js
@@ -15,11 +15,40 @@ module.exports.findOneTournament = (req,res) => {
 }
 
 //create a tournament
-module.exports.createTournament = (req,res) => {
+module.exports.createTournament = (req, res) => {
     Tournament.create(req.body)
-        .then(newTournament => res.json({ tournament: newTournament}))
+        .then(newTournament => {
+            console.log("New tournament created successfully!");
+            res.json({ tournament: newTournament });
+        })
         .catch(err => {
-            console.error('Error:', err); // Add this line for logging
+            console.error('Error:', err);
             res.status(500).json({ message: "Something went wrong", error: err });
+        });
+}
+
+//update or edit tournament
+module.exports.updateTournament = (req, res) => {
+    Tournament.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+        .then(updatedTournament => {
+            console.log("Tournament updated successfully!");
+            res.json({ tournament: updatedTournament });
+        })
+        .catch(err => {
+            console.error('Error:', err);
+            res.json({ message: "Something went wrong", error: err });
+        });
+}
+
+//delete tournament
+module.exports.deleteTournament = (req, res) => {
+    Tournament.findByIdAndDelete(req.params.id)
+        .then(result => {
+            console.log("Tournament deleted successfully!");
+            res.json({ result: result });
+        })
+        .catch(err => {
+            console.error('Error:', err);
+            res.json({ message: "Something went wrong", error: err });
         });
 }
--- server/routes/tournament.routes.js
+++ server/routes/tournament.routes.js
@@ -4,4 +4,6 @@ module.exports = app => {
     app.get('/api/tournaments', TournamentController.findAllTournaments);
     app.get('/api/tournaments/:id', TournamentController.findOneTournament);
     app.post('/api/tournaments', TournamentController.createTournament );
+    app.patch('/api/tournaments/:id', TournamentController.updateTournament);
+    app.delete('/api/tournaments/:id', TournamentController.deleteTournament);
 };
