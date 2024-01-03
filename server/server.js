483f94b3dac4add0a7e844d0c677ab01b96cc761
oscarg11 <oscar.g2511@gmail.com>
2024-01-02 19:48:51 -0500

    added cors to server file

--- server/server.js
+++ server/server.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const app = express();
 const port = 8000;
-// const cors = require('cors');
+
+const cors = require('cors');
+app.use(cors())
+
 require('./config/mongoose.config');
 app.use( express.json() );
 app.use( express.urlencoded({ extended: true}));
