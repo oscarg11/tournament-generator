1fe5ead77b294046e898c4d0ae2d7ab0ef39c5a0
oscarg11 <oscar.g2511@gmail.com>
2024-01-22 23:50:36 -0500

    Conditional rendered form if the user selects group stage with knockout format

--- client/src/components/TournamentForm.jsx
+++ client/src/components/TournamentForm.jsx
@@ -5,19 +5,24 @@ const TournamentForm = () => {
   const [tournamentData, setTournamentData] = useState({
     tournamentName: '',
     format: '',
-    numberOfGroups: '',
-    numberOfMatches:'',
     numberOfParticipants: '',
     participants: [{ firstName: '', lastName: '', teamName: '' }]
   });
 
+// Additional state for the number of legs in group stage
+  const [numberOfGroupStageLegs, setNumberOfGroupStageLegs] = useState(''); 
+
 
   const onChangeHandler = (e) => {
-    setTournamentData({
-      ...tournamentData,
-      [e.target.name]: e.target.value
-    });
-  }
+    if (e.target.name === 'numberOfGroupStageLegs') {
+      setNumberOfGroupStageLegs(e.target.value);
+    } else {
+      setTournamentData({
+        ...tournamentData,
+        [e.target.name]: e.target.value
+      });
+    }
+  };
 
 
   const handleSubmit = (e) => {
@@ -61,25 +66,52 @@ const TournamentForm = () => {
               <option value='league'>League</option>
             </select>
         </div>
-        {/* Conditionally render Number of Groups Dropdown */}
-        {tournamentData.format === 'groupAndKnockout' && (
-          <div className="form-group mb-3">
-            <label htmlFor="numberOfGroups">Number of Groups:</label>
+
+        {/*  Conditional input for number of group stage legs */}
+        { tournamentData.format === 'groupAndKnockout' && (
+          <div className='form-group mb-3'>
+            { errors.numberOfGroups ? <p className="text-danger">{errors.numberOfGroups.message}</p> : "" }
+            <label htmlFor="numberOfGroupStageLegs">Number of Group Stage Legs:</label>
             <select
-              className="form-select"
-              id="numberOfGroups"
-              name="numberOfGroups"
-              onChange={onChangeHandler}
-              value={tournamentData.numberOfGroups}
-            >
-              <option value="">Select Number of Groups</option>
-              {/* Add options for number of groups here */}
-              <option value="2">2</option>
+              className='form-select'
+              id= 'numberOfGroupStageLegs'
+              name='numberOfGroupStageLegs'
+              value={ numberOfGroupStageLegs}
+              onChange={ onChangeHandler }
+              >
+              <option value=''>Select Number of Group Stage Legs</option>
+              <option value='1'>1</option>
+              <option value='2'>2</option>
+              <option value='3'>3</option>
+              <option value="4">4</option>
             </select>
           </div>
         )}
 
+
         {/* Number of Participants */}
+        <div className="form-group mb-3">
+          { errors.numberOfParticipants ? <p className="text-danger">{errors.numberOfParticipants.message}</p> : "" }
+          <label htmlFor="numberOfParticipants">Number of Participants:</label>
+          <select
+            className='form-select'
+            id= 'numberOfParticipants'
+            name='numberOfParticipants'
+            onChange={ onChangeHandler }
+            value={ tournamentData.numberOfParticipants}
+            >
+            <option value=''>Select Number of Participants</option>
+            <option value='4'>4</option>
+            <option value='8'>8</option>
+            <option value='16'>16</option>
+            <option value='24'>24</option>
+            <option value='32'>32</option>
+            <option value="40">40</option>
+            <option value="48">48</option>
+            <option value="56">56</option>
+            <option value="64">64</option>
+          </select>
+        </div>
 
         <button className='btn btn-primary'>Create Tournament</button>
       </form>
