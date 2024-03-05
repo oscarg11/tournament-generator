55efc6f02d93e88d68a2585973dc1d48be9c8129
oscarg11 <oscar.g2511@gmail.com>
2024-03-04 23:08:03 -0500

    implemented boostrap styles to divide the form into two columns

--- client/src/components/TournamentForm.jsx
+++ client/src/components/TournamentForm.jsx
@@ -105,134 +105,141 @@ const TournamentForm = () => {
 
 
   return (
-    <div>
+    <div className='container'>
       <h1>Create your Tournament!</h1>
-      <form action="" className="col-md-6 offset-2" onSubmit={ handleSubmit }>
+      <form action="" className="row" onSubmit={ handleSubmit }>
         {/* Tournament Name */}
-        <div className="form-group mb-3">
-        { errors.tournamentName ? <p className="text-danger">{errors.tournamentName.message}</p> : "" }
-          <label htmlFor="tournamentName">Tournament Name:</label>
-          <input type="text" className="form-control" id="tournamentName" value={tournamentData.tournamentName}
-          placeholder="Enter Tournament Name" name='tournamentName' onChange={ onChangeHandler}/>
-        </div>
+        <div className="col-md-8">
+          <div className="form-group mb-3">
+          { errors.tournamentName ? <p className="text-danger">{errors.tournamentName.message}</p> : "" }
+            <label htmlFor="tournamentName">Tournament Name:</label>
+            <input type="text" className="form-control" id="tournamentName" value={tournamentData.tournamentName}
+            placeholder="Enter Tournament Name" name='tournamentName' onChange={ onChangeHandler}/>
+          </div>
 
-        {/* Format */}
-        <div className="form-group mb-3 dropdown">
-          { errors.format ? <p className="text-danger">{errors.format.message}</p> : "" }
-          <label htmlFor="format">Format:</label>
+          {/* Format */}
+          <div className="form-group mb-3 dropdown">
+            { errors.format ? <p className="text-danger">{errors.format.message}</p> : "" }
+            <label htmlFor="format">Format:</label>
+              <select
+                className='form-select'
+                id= 'format'
+                name='format'
+                onChange={ onChangeHandler }
+                value={ tournamentData.format}
+                >
+                <option value=''>Select Format</option>
+                <option value='groupAndKnockout'>Group and Knockout</option>
+                <option value='knockout'>Knockout</option>
+                <option value='league'>League</option>
+              </select>
+          </div>
+
+          {/*  Conditional input for number of group stage legs */}
+          { tournamentData.format === 'groupAndKnockout' && (
+            <div className='form-group mb-3'>
+              { errors.numberOfGroupStageLegs ? <p className="text-danger">{errors.numberOfGroupStageLegs.message}</p> : "" }
+              <label htmlFor="numberOfGroupStageLegs">Number of Group Stage Legs:</label>
+              <select
+                className='form-select'
+                id= 'numberOfGroupStageLegs'
+                name='numberOfGroupStageLegs'
+                value={ tournamentData.numberOfGroupStageLegs}
+                onChange={ onChangeHandler }
+                >
+                <option value=''>Select Number of Group Stage Legs</option>
+                <option value='1'>1</option>
+                <option value='2'>2</option>
+                <option value='3'>3</option>
+                <option value="4">4</option>
+              </select>
+            </div>
+          )}
+
+          {/* Number of Participants */}
+          <div className="form-group mb-3">
+            { errors.numberOfParticipants ? <p className="text-danger">{errors.numberOfParticipants.message}</p> : "" }
+            <label htmlFor="numberOfParticipants">Number of Participants:</label>
             <select
               className='form-select'
-              id= 'format'
-              name='format'
+              id= 'numberOfParticipants'
+              name='numberOfParticipants'
               onChange={ onChangeHandler }
-              value={ tournamentData.format}
+              value={ tournamentData.numberOfParticipants}
               >
-              <option value=''>Select Format</option>
-              <option value='groupAndKnockout'>Group and Knockout</option>
-              <option value='knockout'>Knockout</option>
-              <option value='league'>League</option>
-            </select>
-        </div>
-
-        {/*  Conditional input for number of group stage legs */}
-        { tournamentData.format === 'groupAndKnockout' && (
-          <div className='form-group mb-3'>
-            { errors.numberOfGroupStageLegs ? <p className="text-danger">{errors.numberOfGroupStageLegs.message}</p> : "" }
-            <label htmlFor="numberOfGroupStageLegs">Number of Group Stage Legs:</label>
-            <select
-              className='form-select'
-              id= 'numberOfGroupStageLegs'
-              name='numberOfGroupStageLegs'
-              value={ tournamentData.numberOfGroupStageLegs}
-              onChange={ onChangeHandler }
-              >
-              <option value=''>Select Number of Group Stage Legs</option>
-              <option value='1'>1</option>
-              <option value='2'>2</option>
-              <option value='3'>3</option>
-              <option value="4">4</option>
+              <option value=''>Select Number of Participants</option>
+              <option value='4'>4</option>
+              <option value='8'>8</option>
+              <option value='16'>16</option>
+              <option value='24'>24</option>
+              <option value='32'>32</option>
+              <option value="40">40</option>
+              <option value="48">48</option>
+              <option value="56">56</option>
+              <option value="64">64</option>
             </select>
           </div>
-        )}
-
-        {/* Number of Participants */}
-        <div className="form-group mb-3">
-          { errors.numberOfParticipants ? <p className="text-danger">{errors.numberOfParticipants.message}</p> : "" }
-          <label htmlFor="numberOfParticipants">Number of Participants:</label>
-          <select
-            className='form-select'
-            id= 'numberOfParticipants'
-            name='numberOfParticipants'
-            onChange={ onChangeHandler }
-            value={ tournamentData.numberOfParticipants}
-            >
-            <option value=''>Select Number of Participants</option>
-            <option value='4'>4</option>
-            <option value='8'>8</option>
-            <option value='16'>16</option>
-            <option value='24'>24</option>
-            <option value='32'>32</option>
-            <option value="40">40</option>
-            <option value="48">48</option>
-            <option value="56">56</option>
-            <option value="64">64</option>
-          </select>
         </div>
-
-        {/* Input fields for Participant's Name & Team Name */}
-        <div className="form-group mb-3">
-          { errors.participantName ? <p className="text-danger">{errors.participantName.message}</p> : "" }
-          <label htmlFor="participantName">Participant's Name:</label>
-          <input
-            type="text"
-            className="form-control"
-            id="participantName"
-            placeholder="Enter Participant's Name"
-            name='participantName' 
-            value={currentParticipant.participantName}
-            onChange={onChangeHandler} />
-          <div>
-            { errors.teamName ? <p className="text-danger">{errors.teamName.message}</p> : "" }
-            <label htmlFor="teamName">Team Name:</label>
-            <input 
-              type="text" 
-              className="form-control" 
-              id="teamName"
-              placeholder="Enter Team Name"
-              name='teamName'
-              value={currentParticipant.teamName}
+        
+        <div className="col-md-4">
+            {/* Input fields for Participant's Name & Team Name */}
+          <div className="form-group mb-3">
+            { errors.participantName ? <p className="text-danger">{errors.participantName.message}</p> : "" }
+            <label htmlFor="participantName">Participant's Name:</label>
+            <input
+              type="text"
+              className="form-control"
+              id="participantName"
+              placeholder="Enter Participant's Name"
+              name='participantName' 
+              value={currentParticipant.participantName}
               onChange={onChangeHandler} />
+            <div>
+              { errors.teamName ? <p className="text-danger">{errors.teamName.message}</p> : "" }
+              <label htmlFor="teamName">Team Name:</label>
+              <input 
+                type="text" 
+                className="form-control" 
+                id="teamName"
+                placeholder="Enter Team Name"
+                name='teamName'
+                value={currentParticipant.teamName}
+                onChange={onChangeHandler} />
+            </div>
+            {/* Button to add a new participant */}
+            <button type="button" className='btn btn-success mt-2' onClick={handleAddParticipant}>Add New Participant</button>
           </div>
-          {/* Button to add a new participant */}
-          <button type="button" className='btn btn-success mt-2' onClick={handleAddParticipant}>Add New Participant</button>
-        </div>
 
-        {/* Display list of participants in a scrollable box */}
-        <div style={{ maxHeight: "1000px", overflowY: "scroll" }}>
-          {}
-          <h3>Participants to Add:</h3>
-          {/* Incomplete participants validtation message */}
-          {errors.incommpleteParticipants && <p className="text-danger">{errors.incommpleteParticipants}</p>}
-          <ol  style={{ listStylePosition: 'inside' }}>
-            {/* Create an array with a length equal to the selected number of participants */}
-            {Array.from({ length: tournamentData.numberOfParticipants }, (_, i) => (
-              <li key={i} style={{ marginBottom: '10px',color: tournamentData.participants[i] ? 'black' : '#999' }}>
-                {/* Check if a participant exists at this index */}
-                {tournamentData.participants[i] ? 
-                 // If a participant exists, display their name and team name
-                  `${tournamentData.participants[i].participantName} (${tournamentData.participants[i].teamName})`
-                  // If a participant does not exist, display a placeholder 
-                  :"Participant Name"}
-                {tournamentData.participants[i] && (
-                  <button type="button" onClick={() => handleDeleteParticipant(i)} style={{ marginLeft: "10px" }}>
-                    Delete
-                  </button>
-                )}
-              </li>
-            ))}
-          </ol>
+          {/* Display list of participants in a scrollable box */}
+          <div style={{ maxHeight: "1000px", overflowY: "scroll" }}>
+            {}
+            <h3>Participants to Add:</h3>
+            {/* Incomplete participants validtation message */}
+            {errors.incommpleteParticipants && <p className="text-danger">{errors.incommpleteParticipants}</p>}
+            <ol  style={{ listStylePosition: 'inside' }}>
+              {/* Create an array with a length equal to the selected number of participants */}
+              {Array.from({ length: tournamentData.numberOfParticipants }, (_, i) => (
+                <li key={i} style={{ marginBottom: '10px',color: tournamentData.participants[i] ? 'black' : '#999' }}>
+                  {/* Check if a participant exists at this index */}
+                  {tournamentData.participants[i] ? 
+                  // If a participant exists, display their name and team name
+                    `${tournamentData.participants[i].participantName} (${tournamentData.participants[i].teamName})`
+                    // If a participant does not exist, display a placeholder 
+                    :"Participant Name"}
+                  {tournamentData.participants[i] && (
+                    <button type="button" onClick={() => handleDeleteParticipant(i)} style={{ marginLeft: "10px" }}>
+                      Delete
+                    </button>
+                  )}
+                </li>
+              ))}
+            </ol>
+          </div>
         </div>
-        <button className='btn btn-primary'>Create Tournament</button>
+        <div className='col-md-12'>
+          <button className='btn btn-primary'>Create Tournament</button>
+        </div>
+  
       </form>
     </div>
   )
