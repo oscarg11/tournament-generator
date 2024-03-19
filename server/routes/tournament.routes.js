e62554d88c6a4df3d22b0fb496b33c0c4f74e82a
oscarg11 <oscar.g2511@gmail.com>
2024-03-18 23:35:51 -0400

    labeled the create tournament route 'create'

--- server/routes/tournament.routes.js
+++ server/routes/tournament.routes.js
@@ -3,7 +3,7 @@ const TournamentController = require("../controllers/tournament.controller")
 module.exports = app => {
     app.get('/api/tournaments', TournamentController.findAllTournaments);
     app.get('/api/tournaments/:id', TournamentController.findOneTournament);
-    app.post('/api/tournaments', TournamentController.createTournament );
+    app.post('/api/tournaments/create', TournamentController.createTournament );
     app.patch('/api/tournaments/:id', TournamentController.updateTournament);
     app.delete('/api/tournaments/:id', TournamentController.deleteTournament);
 };
