683ef65fa8adbd4317f3c61cf1c62c3ea0474585
oscarg11 <oscar.g2511@gmail.com>
2024-03-19 20:14:40 -0400

    Display tournament details on group stage page

--- client/src/components/GroupStage.jsx
+++ client/src/components/GroupStage.jsx
@@ -1,10 +1,22 @@
 import React, {useState, useEffect} from 'react'
+import {useParams} from 'react-router-dom'
 import axios from 'axios'
 
 const GroupStage = ({tournamentName, format, numberOfGroupStageLegs, numberOfParticipants, participants}) => {
 
   return (
-    <div>GroupStage</div>
+    <div>
+      <h1>{tournamentName}</h1>
+      <p>Format: {format}</p>
+      {format === 'groupAndKnockout' && <p>Number of group stage legs: {numberOfGroupStageLegs}</p>}
+
+      <h2>Participants</h2>
+      <ul>
+        {participants.map((participant, index) => ( 
+          <li key={index}>{participant.participantName} - {participant.teamName}</li>
+        ))}
+      </ul>
+    </div>
   )
 }
 
