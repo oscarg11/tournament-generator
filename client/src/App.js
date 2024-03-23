521efcbdb6521d0d5ce3888b411b352cf7d58ee1
oscarg11 <oscar.g2511@gmail.com>
2024-03-22 21:39:35 -0400

    added path for dashboard component

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import { useEffect } from 'react';
 import LandingPage from './components/LandingPage';
 import TournamentForm from './components/TournamentForm';
 import GroupStage from './components/GroupStage';
+import Dashboard from './components/Dashboard';
 
 function App() {
   useEffect(() => {
@@ -17,9 +18,10 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<LandingPage/>} />
-          <Route path='/create-tournament' element={<TournamentForm/>} />
-          <Route path='/group-stage/:tournamentId' element={<GroupStage/>}/>
+          <Route path='/' element={ <LandingPage/> } />
+          <Route path='/create-tournament' element={ <TournamentForm/> } />
+          <Route path='/dashboard' element= { <Dashboard/> }/>
+          <Route path='/group-stage/:tournamentId' element={ <GroupStage/> }/>
         </Routes>
       </BrowserRouter>
     </div>
