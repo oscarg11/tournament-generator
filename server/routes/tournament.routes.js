7806528a54f42c68f68869e7a576dde855fdd9ab
oscarg11 <oscar.g2511@gmail.com>
2024-03-26 23:39:37 -0400

    updated route for tournament hub component

--- server/routes/tournament.routes.js
+++ server/routes/tournament.routes.js
@@ -2,7 +2,7 @@ const TournamentController = require("../controllers/tournament.controller")
 
 module.exports = app => {
     app.get('/api/tournaments/all-tournaments', TournamentController.findAllTournaments);
-    app.get('/api/tournament/group-stage/:_id', TournamentController.findOneTournament);
+    app.get('/api/tournament-hub/:_id', TournamentController.findOneTournament);
     app.post('/api/tournaments/create-tournament', TournamentController.createTournament );
     app.patch('/api/tournaments/:id', TournamentController.updateTournament);
     app.delete('/api/tournaments/:id', TournamentController.deleteTournament);
