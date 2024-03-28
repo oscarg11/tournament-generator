e4f0def9da71e49011fb09107e838d936ea1347e
oscarg11 <oscar.g2511@gmail.com>
2024-03-27 23:48:48 -0400

    created embedded document for match structures

--- server/models/tournament.model.js
+++ server/models/tournament.model.js
@@ -1,5 +1,22 @@
 const mongoose = require('mongoose');
 
+const MatchSchema = new mongooose.Schema({
+    participants: [
+        {
+            participantName: String,
+            teamName: String,
+            score: Number,
+        },
+        {
+            participantName: String,
+            teamName: String,
+            score: Number,
+        }
+    ],
+    matchNumber: Number,
+    group: String,
+});
+
 const TournamentSchema = new mongoose.Schema({
     tournamentName:{
         type: String,
