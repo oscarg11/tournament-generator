f5b3ffa9dc80d6038743749b1e92714a9d7bc077
oscarg11 <oscar.g2511@gmail.com>
2024-08-05 23:29:14 -0400

    resolved mongodb connection bug

--- server/config/mongoose.config.js
+++ server/config/mongoose.config.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
-
+require('dotenv').config();
 const MOGO_URI = process.env.MONGO_URI;
 const JWT_SECRET = process.env.JWT_SECRET;
 
+console.log("MONGO_URI", MOGO_URI);
 mongoose.connect(MOGO_URI, {
 })
     .then(() => console.log('Established a connection to the database'))
