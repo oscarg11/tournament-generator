d4ce7e77b50007fac52ca55a32c606f5a339ec60
oscarg11 <oscar.g2511@gmail.com>
2025-01-02 22:24:57 -0500

    added add participant route and updated start script

--- server/routes/tournament.routes.js
+++ server/routes/tournament.routes.js
@@ -1,16 +1,21 @@
 const TournamentController = require("../controllers/tournament.controller")
+const ParticipantController = require("../controllers/participant.controller")
 
 module.exports = app => {
     app.get('/api/tournaments/all-tournaments', TournamentController.findAllTournaments);
     app.get('/api/tournament-hub/:_id', TournamentController.findOneTournament);
+    console.log("TournamentController", TournamentController)
     app.post('/api/tournaments/create-tournament', TournamentController.createTournament );
     app.patch('/api/tournaments/:id', TournamentController.updateTournament);
     app.delete('/api/tournaments/:id', TournamentController.deleteTournament);
-
+    
+    //save participants
+    app.post('/api/participants/create-participant', ParticipantController.createParticipant);
+    
     // save matches
     app.patch('/api/tournaments/:id/save-matches', TournamentController.saveMatches);
-
-
+    
+    
     // update match scores
     app.put('/api/tournaments/:tournamentId/matches/:roundIndex/:matchIndex', TournamentController.updateGroupStageMatchScores);
 };
