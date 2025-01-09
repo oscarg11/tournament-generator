b949b632a940e8f6baae4fc6acac38559b7b55de
oscarg11 <oscar.g2511@gmail.com>
2025-01-08 23:03:33 -0500

    added NavBar

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import NavBar from '../components/NavBar'
 import { Link } from 'react-router-dom'
 
 const HomePage = () => {
   return (
     <div>
+        <NavBar/>
         <h1>Welcome to the Tournament Manager</h1>
         <p>Click the button below to create a new tournament</p>
         <Link to="/create-tournament">
