5419dd8248204942662314aedb034033473e9a4c
oscarg11 <oscar.g2511@gmail.com>
2025-02-20 23:30:39 -0500

    modify groups.map to work with participants array instead

--- client/src/components/GroupStandings.jsx
+++ client/src/components/GroupStandings.jsx
@@ -2,47 +2,58 @@ import React from 'react'
 
 
 const GroupStandings = ({groups = []}) => {
+  if (!Array.isArray(groups)) {
+    console.error("Error: `groups` is not an array", groups);
+    return <div>Error loading groups.</div>;
+  }
   return (
     <div>
-        { groups.map((group, groupIndex) => (
-      <div key={ groupIndex } className='mb-3 container'>
-        <div className='row'>
-            <table className='table table-bordered'>
+       {groups.map((group, groupIndex) => {
+        if (!Array.isArray(group.participants)) {
+          console.error(`Error: participant at index ${groupIndex} is not an array`, group.participants);
+          return null; // Skip rendering this group
+        }
+
+        return (
+          <div key={group._id} className='mb-3 container'>
+            <div className='row'>
+              <table className='table table-bordered'>
                 <thead className='table-active'>
-                <tr>
-                    <th scope='"col'>Group { String.fromCharCode(65 + groupIndex) } </th>
-                    <th scope='col'>P</th>
-                    <th scope='col'>W</th>
-                    <th scope='col'>D</th>
-                    <th scope='col'>L</th>
-                    <th scope='col'>GS</th>
-                    <th scope='col'>GA</th>
-                    <th scope='col'>GD</th>
-                    <th scope='col'>PTS</th>
+                  <tr>
+                    <th scope="col">Group {group.groupName}</th>
+                    <th scope="col">P</th>
+                    <th scope="col">W</th>
+                    <th scope="col">D</th>
+                    <th scope="col">L</th>
+                    <th scope="col">GS</th>
+                    <th scope="col">GA</th>
+                    <th scope="col">GD</th>
+                    <th scope="col">PTS</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {group.participants.map((participant, index) => (
+                    <tr key={index}>
+                      <td className='col-2'>{participant.participantName} ({participant.teamName})</td>
+                      <td>{participant?.matchesPlayed || 0}</td>
+                      <td>{participant?.wins || 0}</td>
+                      <td>{participant?.draws || 0}</td>
+                      <td>{participant?.losses || 0}</td>
+                      <td>{participant?.goalsScored || 0}</td>
+                      <td>{participant?.goalsAgainst || 0}</td>
+                      <td>{participant?.goalDifference || 0}</td>
+                      <td>{participant?.points || 0}</td>
                     </tr>
-                    </thead>
-                    <tbody key = { groupIndex }>
-                    
-                    {group.map((participant, index) => (
-                        <tr key={index}>
-                        <td className='col-2'>{participant.participantName} ({participant.teamName})</td>
-                        <td>{participant?.matchesPlayed || 0}</td>
-                        <td>{participant?.wins || 0}</td>
-                        <td>{participant?.draws || 0}</td>
-                        <td>{participant?.losses || 0}</td>
-                        <td>{participant?.goalsScored || 0}</td>
-                        <td>{participant?.goalsAgainst || 0}</td>
-                        <td>{participant?.goalDifference || 0}</td>
-                        <td>{participant?.points || 0}</td>
-                        </tr>
-                    ))}
-                    </tbody>
-            </table>
-        </div>
-      </div>
-        ))}
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        );
+      })}
     </div>
-  )
-}
+  );
+};
+
 
 export default GroupStandings
