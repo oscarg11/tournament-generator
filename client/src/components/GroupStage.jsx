4e0e3691421708ef7f2a3c3cd715c4e7167f0cb5
oscarg11 <oscar.g2511@gmail.com>
2025-03-11 23:28:40 -0400

    set conditional rendering for Group Standings and Group Matches components

--- client/src/components/GroupStage.jsx
+++ client/src/components/GroupStage.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React, {useState} from 'react'
+import GroupStandings from '../components/GroupStandings'
+import GroupMatches from '../components/GroupMatches'
 
-const GroupStage = () => {
+const GroupStage = ({tournamentData, setTournamentData, matchData, setMatchData}) => {
+  // state to track which component to display
+  const [active, setActive] = useState(true)
+
   return (
-    <div>GroupStage</div>
-  )
+    <div className="container">
+      <nav className="navbar navbar-expand navbar-dark bg-secondary mb-3">
+          <div className="collapse navbar-collapse justify-content-start" id="navbarNav">
+            <ul className="navbar-nav">
+                <li className="nav-item">
+                    <button className="btn btn-light mx-2"  onClick={() => setActive(true)}>
+                      Group Standings
+                    </button>
+                </li>
+                <li className="nav-item">
+                <button className="btn btn-light mx-2" onClick={() => setActive(false)}>
+                      Group Matches
+                    </button>
+                </li>
+            </ul>
+          </div>
+        </nav>
+
+            <div className="flex flex-row gap-3">
+                <>
+                  {active? <GroupStandings groups={tournamentData.groups}/> 
+                  : 
+                  <GroupMatches
+                          tournamentData={tournamentData}
+                          setTournamentData={setTournamentData}
+                          matchData={matchData}
+                          setMatchData={setMatchData}
+                  />}
+                </>
+            </div>
+    </div>
+)
 }
 
 export default GroupStage
