d86297a780930c8d547a6d3d6e7fd55e461758f0
oscarg11 <oscar.g2511@gmail.com>
2025-03-14 00:30:29 -0400

    created new components for Dashboard tabs (finals stage, statsTab, teamstab)

---
+++ client/src/components/StatsTab.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const StatsTab = () => {
+  return (
+    <div>
+        <h1>Stats Tab!</h1>
+    </div>
+  )
+}
+
+export default StatsTab
---
+++ client/src/components/TeamsTab.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TeamsTab = () => {
+  return (
+    <div>
+        <h1>Teams Tab!</h1>
+    </div>
+  )
+}
+
+export default TeamsTab
