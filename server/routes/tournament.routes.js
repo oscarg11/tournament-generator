9f886909aa01e7f0ff4a7fe9b886daa21e492d6b
oscarg11 <oscar.g2511@gmail.com>
2025-03-17 23:58:54 -0400

    cleaned up routes file

--- server/routes/tournament.routes.js
+++ server/routes/tournament.routes.js
@@ -3,7 +3,6 @@ const TournamentController = require("../controllers/tournament.controller")
 module.exports = app => {
     app.get('/api/tournaments/all-tournaments', TournamentController.findAllTournaments);
     app.get('/api/dashboard/:id', TournamentController.findOneTournament);
-    console.log("TournamentController", TournamentController)
     app.post('/api/tournaments/create-tournament', TournamentController.createTournament );
     app.patch('/api/tournaments/:id', TournamentController.updateTournament);
     app.delete('/api/tournaments/:id', TournamentController.deleteTournament);
@@ -12,6 +11,6 @@ module.exports = app => {
     app.patch('/api/tournaments/:id/save-matches', TournamentController.saveMatches);
     app.get('/api/tournaments/:id/group-stage-matches', TournamentController.getGroupStageMatches);
     
-    // update match scores
-    app.put('/api/tournaments/:tournamentId/matches/:roundIndex/:matchIndex', TournamentController.updateGroupStageMatchScores);
+    // update group stage match scores
+    app.put('/api/tournaments/:tournamentId/group-matches/:roundIndex/:matchIndex', TournamentController.updateGroupStageMatchScores);
 };
