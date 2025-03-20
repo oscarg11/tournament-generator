2f0dbe5392fdf235aabcc8b06a38868a7f59a60d
oscarg11 <oscar.g2511@gmail.com>
2025-03-19 23:30:58 -0400

    updated determineGroupMatches function with new name and added caculations for goalDifference

--- server/helpers/tournamentFunctions.js
+++ server/helpers/tournamentFunctions.js
@@ -104,48 +104,79 @@ const createGroupStageMatches = (groups, numberOfGroupStageLegs) => {
 
 
 
-    //determin match result
-const determineMatchResult = (participant1, participant2, score,match) => {
+    //determin Group match result
+const determineGroupMatchResult = (participant1, participant2, score) => {
         const POINTS_PER_WIN = 3;
         const POINTS_PER_DRAW = 1;
         const POINTS_PER_LOSS = 0;
 
         console.log("Before Match updates");
-        console.log("Participant1", participant1);
-        console.log("Participant2", participant2);
+       // console.log("Participant1", participant1);
+        //console.log("Participant2", participant2);
+        console.log("Received Score Object:", score);
+    console.log("Participant 1 Score:", score.participant1);
+    console.log("Participant 2 Score:", score.participant2);
+        console.log("Participant 1 - Goals Scored:", participant1.goalsScored, "Goals Against:", participant1.goalsAgainst);
+            console.log("Participant 2 - Goals Scored:", participant2.goalsScored, "Goals Against:", participant2.goalsAgainst);
         
         //participant 1 wins
         if(score.participant1 > score.participant2){
-            participant1.points += POINTS_PER_WIN;
+            participant1.matchesPlayed += 1;
             participant1.wins += 1;
-            participant2.losses += 1;
-            participant2.points += POINTS_PER_LOSS;
-
+            participant1.goalsScored += score.participant1;
+            participant1.goalsAgainst += score.participant2;
+            participant1.goalDifference = participant1.goalsScored - participant1.goalsAgainst;
+            participant1.points += POINTS_PER_WIN;
             participant1.matchHistory.push("W")
+
+            participant2.matchesPlayed += 1;
+            participant2.losses += 1;
+            participant2.goalsScored += score.participant2;
+            participant2.goalsAgainst += score.participant1;
+            participant2.goalDifference = participant2.goalsScored - participant2.goalsAgainst;
+            participant2.points += POINTS_PER_LOSS;
             participant2.matchHistory.push("L")
+        
+        //participant 2 wins
         } else if(score.participant1 < score.participant2){
-            //participant 2 wins
-            participant2.points += POINTS_PER_WIN;
+            participant2.matchesPlayed += 1;
             participant2.wins += 1;
-            participant1.losses += 1;
-            participant1.points += POINTS_PER_LOSS;
-
-            participant1.matchHistory.push("L")
+            participant2.goalsScored += score.participant2;
+            participant2.goalsAgainst += score.participant1;
+            participant2.goalDifference = participant2.goalsScored - participant2.goalsAgainst;
+            participant2.points += POINTS_PER_WIN;
             participant2.matchHistory.push("W")
-        }else{
-            //draw
-            participant1.points += POINTS_PER_DRAW;
-            participant2.points += POINTS_PER_DRAW;
-            participant1.draws += 1;
-            participant2.draws += 1;
 
+            participant1.matchesPlayed += 1;
+            participant1.losses += 1;
+            participant1.goalsScored += score.participant1;
+            participant1.goalsAgainst += score.participant2;
+            participant1.goalDifference = participant1.goalsScored - participant1.goalsAgainst;
+            participant1.points += POINTS_PER_LOSS;
+            participant1.matchHistory.push("L")
+
+        //draw
+        }else{
+            participant1.matchesPlayed += 1;
+            participant1.draws += 1;
+            participant1.goalsScored += score.participant1;
+            participant1.goalsAgainst += score.participant2;
+            participant1.goalDifference = participant1.goalsScored - participant1.goalsAgainst;
+            participant1.points += POINTS_PER_DRAW;
             participant1.matchHistory.push("D")
+
+            participant2.matchesPlayed += 1;
+            participant2.draws += 1;
+            participant2.goalsScored += score.participant2;
+            participant2.goalsAgainst += score.participant1;
+            participant2.goalDifference = participant2.goalsScored - participant2.goalsAgainst;
+            participant2.points += POINTS_PER_DRAW;
             participant2.matchHistory.push("D")
         }
 
-        console.log("After Match updates");
-        console.log("Participant1", participant1);
-        console.log("Participant2", participant2);
+        console.log("After Update:");
+        console.log("Participant 1 - Goals Scored:", participant1.goalsScored, "Goals Against:", participant1.goalsAgainst, "Goal Difference:", participant1.goalDifference);
+        console.log("Participant 2 - Goals Scored:", participant2.goalsScored, "Goals Against:", participant2.goalsAgainst, "Goal Difference:", participant2.goalDifference);
         
         return {participant1, participant2}
 }
@@ -156,5 +187,5 @@ module.exports = {
     createGroups,
     shuffle,
     createGroupStageMatches,
-    determineMatchResult
+    determineGroupMatchResult
 }
