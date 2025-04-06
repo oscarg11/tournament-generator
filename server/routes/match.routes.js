5106c89c7f15075ecce30836107c4a41e5f07a55
oscarg11 <oscar.g2511@gmail.com>
2025-04-06 18:22:11 -0400

    created a match routes file and moved match related routes from tournament routes into it

---
+++ server/routes/match.routes.js
@@ -0,0 +1,12 @@
+const MatchController = require('../controllers/match.controller');
+
+module.exports = app => {
+    // save matches
+    app.patch('/api/tournaments/:id/save-matches', MatchController.saveMatches);
+    
+    app.get('/api/tournaments/:id/group-stage-matches', MatchController.getGroupStageMatches);
+    
+    // update group stage match scores
+    app.put('/api/tournaments/:tournamentId/group-matches/:roundIndex/:matchIndex', MatchController.updateGroupStageMatchScores);
+}
+
--- server/routes/tournament.routes.js
+++ server/routes/tournament.routes.js
@@ -6,11 +6,4 @@ module.exports = app => {
     app.post('/api/tournaments/create-tournament', TournamentController.createTournament );
     app.patch('/api/tournaments/:id', TournamentController.updateTournament);
     app.delete('/api/tournaments/:id', TournamentController.deleteTournament);
-    
-    // save matches
-    app.patch('/api/tournaments/:id/save-matches', TournamentController.saveMatches);
-    app.get('/api/tournaments/:id/group-stage-matches', TournamentController.getGroupStageMatches);
-    
-    // update group stage match scores
-    app.put('/api/tournaments/:tournamentId/group-matches/:roundIndex/:matchIndex', TournamentController.updateGroupStageMatchScores);
 };
