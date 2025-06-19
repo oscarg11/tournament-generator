5bf6cdb5092339f173e2e646e06bf48294bd4c6f
oscarg11 <oscar.g2511@gmail.com>
2025-06-19 15:29:05 -0400

    added a route to update knockout match

--- server/routes/match.routes.js
+++ server/routes/match.routes.js
@@ -6,9 +6,15 @@ module.exports = app => {
     
     // get group stage matches
     app.get('/api/tournaments/:id/group-stage-matches', MatchController.getGroupStageMatches);
+
+    //get knockout stage matches
+    app.get('/api/tournaments/:id/knockout-stage-matches', MatchController.getKnockoutStageMatches);
     
     // update group stage match scores
     app.put('/api/tournaments/:tournamentId/group-matches/:roundIndex/:matchIndex', MatchController.updateGroupStageMatchScores);
+
+    // update knockout stage match scores
+    app.put('/api/tournaments/:tournamentId/knockout-matches/:stageName/:matchIndex', MatchController.updateKnockoutStageMatch);
 
     // reset group stage match
     app.put('/api/tournaments/:tournamentId/reset-group-match/:roundIndex/:matchIndex', MatchController.resetGroupStageMatch);
