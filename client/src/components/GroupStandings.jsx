83792fc068053142e758c4dc753a73f33ce97b58
oscarg11 <oscar.g2511@gmail.com>
2025-12-24 17:52:07 -0500

    refactored to be able to pull group data from the participants data

--- client/src/components/GroupStandings.jsx
+++ client/src/components/GroupStandings.jsx
@@ -2,28 +2,40 @@ import { useOutletContext } from 'react-router-dom'
 
 const GroupStandings = () => {
   const { tournamentData } = useOutletContext();
-  const groups = tournamentData.groups || [];
+  
+  //group participants by group name
+  console.log("Participants in GroupStandings:", tournamentData.participants);
+  
+  const participants = tournamentData.participants || [];
 
-  if (!Array.isArray(groups)) {
-    console.error("Error: `groups` is not an array", groups);
-    return <div>Error loading groups.</div>;
-  }
+  const participantsByGroup = participants.reduce((acc, participant) => {
+    const groupName = participant.groupName || 'Ungrouped';
+    if(!acc[groupName]){
+      acc[groupName] = [];
+    }
+    acc[groupName].push(participant);
+    return acc;
+  }, {})
+
+  console.log("Participants grouped by groupName:", participantsByGroup);
+
 
   return (
     <div>
-      {groups.map((group, groupIndex) => {
-        if (!Array.isArray(group.participants)) {
-          console.error(`Error: participant at index ${groupIndex} is not an array`, group.participants);
+      {Object.entries(participantsByGroup).map(
+        ([groupName, participants], groupIndex) => {
+        if (!Array.isArray(participants)) {
+          console.error(`Error: participant at index ${groupIndex} is not an array`, participants);
           return null; // Skip rendering this group
         }
 
         return (
-          <div key={group._id} className='mb-3 container'>
+          <div key={groupName} className='mb-3 container'>
             <div className='row'>
               <table className='table table-bordered'>
                 <thead className='table-active'>
                   <tr>
-                    <th scope="col">Group {group.groupName}</th>
+                    <th scope="col">Group {groupName}</th>
                     <th scope="col">MP</th>
                     <th scope="col">W</th>
                     <th scope="col">D</th>
@@ -36,7 +48,7 @@ const GroupStandings = () => {
                   </tr>
                 </thead>
                 <tbody>
-                  {group.participants.map((participant, index) => (
+                  {participants.map((participant, index) => (
                     <tr key={participant._id || participant.participantName}>
 
                       <td className='col-2'>
